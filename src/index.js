beb62d9de750a8aa55fc4d5777d05d629ee3808f
Prateek Gogia <gogia.prateek@hotmail.com>
2020-02-02 11:05:32 +0530

    Initial Commit, Amigos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import Root from './Root';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './index.scss';
+
+ReactDOM.render(
+  <Root>
+    <App />
+  </Root>,
+  document.getElementById('root')
+);
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,11 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new adapter() });
+
+/* Globals only for tests */
+global.React = React;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
